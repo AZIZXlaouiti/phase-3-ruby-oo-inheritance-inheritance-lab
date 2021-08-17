aa1eded471a4dab3000dc20d5bac19b25a1bb079
AZIZXlaouiti <azizrocke@gmail.com>
2021-08-17 18:28:56 -0400

    completed

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,12 @@
-class Student 
-
+class Student < User
+    attr_reader :knowledge
+   def initialize
+     super 
+     @knowledge = [] 
+   end
+   def learn(name)
+      @knowledge << name
+    end
+    
+   
 end
